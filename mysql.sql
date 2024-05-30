59f9ee1ed7ede6860dc09fcecd2edc66a3840032
xiao-youbang <9161383@qq.com>
2024-05-30 14:31:04 +0800

    Update README.md

--- mysql.sql
+++ mysql.sql
@@ -9,4 +9,5 @@ create table user (
 );
 
 # be sure there is at least one user!
-insert into user values (0, 'admin', '123456', 1);
+# Password is '123456'
+insert into user values (0, 'admin', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', 1);
